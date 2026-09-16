08c3766ce153967f06dfea0cd0ae935d011f1951
chrishenn <chenn@alum.mit.edu>
2026-09-16 15:45:25 -0500

    feat: replace chezmoi with mise bootstrap

--- unattend.ps1
+++ unattend.ps1
@@ -189,7 +189,8 @@ function unattend2 ([hashtable] $opt) {
     cfg_autologin $opt.cfgfile
     cfg_scoop_prv $opt.cfg $opt.cfgfile
 
-    chezmoi init chrishenn --apply --force --promptDefaults
+    $env:GITHUB_TOKEN = (op read "op://homelab/github/credential")
+    mise bootstrap --from git@github.com:chrishenn/bootstrap.git --from-dir ~/Projects/bootstrap --skip-dirty --update -y
     & $opt.amecli $opt.playbook [string]$opt.ameargs
 
     write-host -f green 'main 2 done'
